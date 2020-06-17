806d3be50bcfccb6140e843ce624a45e21d804f0
Pascal Cañadas <pascal@mirandais.fr>
2020-06-17 19:09:36 +0200

    First commit

---
+++ FullScreen.js
@@ -0,0 +1,45 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ImageFullBig from './icons/fullscreen-big.svg';
+import ImageFullCancel from './icons/fullscreen-cancel.svg';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import './css/style.css';
+
+export default class FullScreen extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'fullScreen', locale => {
+            const view = new ButtonView( locale );
+            let etat = 0; //si 0 position normale
+            view.set( {
+                label: 'Plein écran',
+                icon: ImageFullBig,
+                tooltip: true
+            } );
+
+            // Callback executed once the image is clicked.
+            view.on( 'execute', () => {
+                if(etat==1){
+                    editor.sourceElement.nextElementSibling.removeAttribute('id');
+                    view.set( {
+                        label: 'Plein écran',
+                        icon: ImageFullBig,
+                        tooltip: true
+                    } );
+                    etat=0;
+                }else{
+                    editor.sourceElement.nextElementSibling.setAttribute("id", 'fullscreeneditor');
+                    view.set( {
+                        label: 'Mode Normal',
+                        icon: ImageFullCancel,
+                        tooltip: true
+                    } );
+                    etat=1;
+                }
+            
+            } );
+
+            return view;
+        } );
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+Fullscreen for CKEditor 5 classic editor build
+
+
+```
+npm i -D @ckeditor/ckeditor5-core
+npm i -D @ckeditor/ckeditor5-ui
+```
+
+
+
+Include in your ckeditor.js
+
+```
+import FullScreen from './fullscreen/FullScreen';
+```
+
+Include in the plugin List
+ClassicEditor.builtinPlugins = [
+	....
+	FullScreen,
+];
+```
+
+And include it in your toolbar
+```
+ClassicEditor.defaultConfig = {
+	toolbar: {
+		items: [
+			.....,
+			'fullScreen'
+		]
+	},
+```
